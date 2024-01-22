680d20eb6efb13ce1c00cd70923a794e3800ce5d
André Zanghelini <An-dz@users.noreply.github.com>
2024-01-22 09:28:19 -0300

    chg: remove JOJO token

--- src/types/currency.ts
+++ src/types/currency.ts
@@ -1,13 +1,11 @@
 export enum Currency {
   USDC = 'USDC',
   BRZ = 'BRZ',
-  JOJO = 'JOJO',
   BIB01 = 'BIB01',
 }
 
 export const currencyDecimals: Record<Currency, number> = {
   [Currency.USDC]: 6,
   [Currency.BRZ]: 18,
-  [Currency.JOJO]: 18,
   [Currency.BIB01]: 18,
 };
--- src/utils/currencyToTokenAddress.ts
+++ src/utils/currencyToTokenAddress.ts
@@ -11,8 +11,6 @@ export function currencyToTokenAddress(currency: Currency): `0x${string}` {
       return process.env.NODE_ENV === 'production'
         ? '0x4eD141110F6EeeAbA9A1df36d8c26f684d2475Dc'
         : '0x35928a20EfA22EA35dCde06Ac201440aAd2fEC05';
-    case Currency.JOJO:
-      return '0x907F41fE794Fe0e3562C2c3B4F35D782aDfF225F';
     case Currency.BIB01:
       return '0xA5c445c85089FF8AbF5C960842a4cF23669AE6D4';
   }
