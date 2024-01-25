5d5bd843c3b517743c87dbe696d70b5944f34370
Thalles Passos <contato@thalles.me>
2024-01-25 15:07:35 -0300

    fix: this should be a hexadecimal type

--- src/graphql/models/WithdrawModel.ts
+++ src/graphql/models/WithdrawModel.ts
@@ -1,11 +1,12 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { HexadecimalScalar } from '../scalars/Hexadecimal';
 import { StableCurrency } from 'src/types/currency';
+import { Hexadecimal } from 'src/types/hexadecimal';
 
 @ObjectType()
 export class WithdrawModel {
   @Field(() => HexadecimalScalar)
-  blockchainDepositAddress: string;
+  blockchainDepositAddress: Hexadecimal;
   @Field(() => String)
   expiresAt: string;
   @Field(() => String)
