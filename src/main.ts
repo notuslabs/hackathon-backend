a892e19eafe1101303ebb9a5e4661fd330387436
Thalles Passos <contato@thalles.me>
2024-02-01 10:26:40 -0300

    refactor: move sentry's DSN to env

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,19 @@ import { AppModule } from "./app.module";
 import * as Sentry from "@sentry/node";
 
 Sentry.init({
-	dsn: "https://0829957158caa4be27c3cb9648a5d520@o4506514324848640.ingest.sentry.io/4506554583613440",
+	dsn: process.env.SENTRY_DSN,
 	tracesSampleRate: 1.0,
 	beforeSend(event) {
 		for (const breadcumb of event.breadcrumbs ?? []) {
 			if (breadcumb.data?.url.includes(process.env.ALCHEMY_HTTP_API_URL)) {
 				breadcumb.data.url =
-					"https://redacted-client-side-url.com/but/its/a/ether/node";
+					"https://redacted-client-side-url.com/but/this/is/a/polygon/node";
 			}
 		}
 
 		return event;
 	},
+	enabled: process.env.SENTRY_DSN !== undefined,
 });
 
 async function bootstrap() {
