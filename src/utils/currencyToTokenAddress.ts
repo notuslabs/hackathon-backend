fd6a01912a222c14beffbd67ac66f16e0f9d0642
André Zanghelini <An-dz@users.noreply.github.com>
2024-02-05 17:46:08 -0300

    fix: wrong token addresses

--- src/utils/currencyToTokenAddress.ts
+++ src/utils/currencyToTokenAddress.ts
@@ -8,8 +8,8 @@ import { Hexadecimal } from "src/types/hexadecimal";
 type Currency = InvestCurrency | StableCurrency | AllCurrency;
 
 const tokenToSymbolList: Record<Currency, Hexadecimal> = {
-  USDC: "0xd6df932a45c0f255f85145f286ea0b292b21c90b",
-  BRZ: "0x3c499c542cef5e3811e1192ce70d8cc03d5c3359",
+  USDC: "0x3c499c542cef5e3811e1192ce70d8cc03d5c3359",
+  BRZ: "0x4eD141110F6EeeAbA9A1df36d8c26f684d2475Dc",
   // TODO: For now, Backed doesn't have the token on Polygon's mainnet. We'll use the same token address as DAI
   BIB01: "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063",
 } as const;
