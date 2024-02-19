12fd3f6a9939bdbed6cd366281143d7c8bdd62dc
André Zanghelini <An-dz@users.noreply.github.com>
2024-02-19 17:36:01 -0300

    fix: Particle hates permissionless

--- src/services/ExecuteUserOperationService.ts
+++ src/services/ExecuteUserOperationService.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { ENTRY_POINT_ADDRESS } from "src/constants";
 import { UserOperation } from "src/types/useroperation";
 import { bundlerClient } from "src/utils/clients";
+import { avalanche } from "viem/chains";
 
 export type ExecuteUserOperationInput = {
 	userOperations: UserOperation[];
@@ -12,11 +13,36 @@ export class ExecuteUserOperationService {
 	async execute({ userOperations }: ExecuteUserOperationInput) {
 		const userOpReceipts = await Promise.all(
 			userOperations.map(async (userOperation) => {
-				return bundlerClient.waitForUserOperationReceipt({
-					hash: await bundlerClient.sendUserOperation({
-						userOperation,
-						entryPoint: ENTRY_POINT_ADDRESS,
+				const options = {
+					method: "POST",
+					headers: {
+						accept: "application/json",
+						"content-type": "application/json",
+					},
+					body: JSON.stringify({
+						method: "eth_sendUserOperation",
+						params: [
+							{
+								...userOperation,
+								callGasLimit: `0x${userOperation.callGasLimit.toString(16)}`,
+								preVerificationGas: `0x${userOperation.preVerificationGas.toString(16)}`,
+								verificationGasLimit: `0x${userOperation.verificationGasLimit.toString(16)}`,
+								maxFeePerGas: `0x${userOperation.maxFeePerGas.toString(16)}`,
+								maxPriorityFeePerGas: `0x${userOperation.maxPriorityFeePerGas.toString(16)}`,
+							},
+							ENTRY_POINT_ADDRESS,
+						],
+						id: 1695717470,
+						jsonrpc: "2.0",
+						chainId: avalanche.id,
 					}),
+				};
+
+				const a = await fetch("https://bundler.particle.network/", options);
+				const response = await a.json();
+
+				return bundlerClient.waitForUserOperationReceipt({
+					hash: response.result,
 				});
 			}),
 		);
